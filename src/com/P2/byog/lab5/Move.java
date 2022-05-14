12cdcbf9ed4be2f75c1e08bb557ced9338724a98
cpf2021-gif <2992247892@qq.com>
2022-05-14 10:36:20 +0800

    Added player movement capability.

---
+++ src/com/P2/byog/lab5/Move.java
@@ -0,0 +1,97 @@
+package com.P2.byog.lab5;
+
+import com.P2.byog.TileEngine.TERenderer;
+import com.P2.byog.TileEngine.TETile;
+import com.P2.byog.TileEngine.Tileset;
+import edu.princeton.cs.introcs.StdDraw;
+
+import java.awt.*;
+
+public class Move {
+    private static final int WIDTH = 50; // x
+    private static final int HEIGHT = 50; // y
+    private static  int player_x;
+    private static  int player_y;
+    private static  int target_x;
+    private static  int target_y;
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        Map.initialize(world,WIDTH,HEIGHT);
+        Map.connect(world,WIDTH,HEIGHT);
+        getPosition(Map.SelectPoint(world));
+        ter.renderFrame(world);
+        move(world,ter);
+        drawFrame("Win!!!");
+    }
+
+    public static void getPosition(int[][] p) {
+        player_x = p[0][0];
+        player_y = p[0][1];
+        target_x = p[1][0];
+        target_y = p[1][1];
+    }
+
+    public static boolean isWin() {
+        return player_x == target_x && player_y == target_y;
+    }
+
+    public static void move(TETile[][] w, TERenderer ter) {
+        while (!isWin()) {
+            if(!StdDraw.hasNextKeyTyped())
+                continue;
+            char ch = StdDraw.nextKeyTyped();
+            int[] p = movePoint(ch);
+            int x = p[0], y = p[1];
+            if(Map.isOk(x,y) && (w[x][y] == Tileset.NOTHING || w[x][y] == Tileset.LOCKED_DOOR)) {
+                swap(x, y, player_x, player_y, w);
+                player_x = x;
+                player_y = y;
+                ter.renderFrame(w);
+                StdDraw.pause(50);
+            }
+        }
+    }
+
+    public static int[] movePoint(char ch) {
+        switch (ch) {
+            case 'w':
+                return new int[]{player_x,player_y + 1};
+            case 's':
+                return new int[]{player_x,player_y - 1};
+            case 'a':
+                return new int[]{player_x - 1,player_y};
+            case 'd':
+                return new int[]{player_x + 1,player_y};
+                default:
+                    return new int[]{player_x,player_y};
+        }
+    }
+
+    private static void swap(int x, int y, int px, int py, TETile[][] w) {
+        TETile temp = w[x][y];
+        w[x][y] = w[px][py];
+        w[px][py] = temp;
+        if(w[px][py] == Tileset.LOCKED_DOOR) {
+            w[px][py] = Tileset.NOTHING;
+        }
+    }
+
+    public static void  drawFrame(String s) {
+        int midWidth = WIDTH / 2;
+        int midHeight = HEIGHT / 2;
+
+        StdDraw.clear();
+        StdDraw.clear(Color.black);
+
+        // Draw the actual text
+        Font bigFont = new Font("Monaco", Font.BOLD, 30);
+        StdDraw.setFont(bigFont);
+        StdDraw.setPenColor(Color.white);
+        StdDraw.text(midWidth, midHeight, s);
+        StdDraw.show();
+    }
+}
